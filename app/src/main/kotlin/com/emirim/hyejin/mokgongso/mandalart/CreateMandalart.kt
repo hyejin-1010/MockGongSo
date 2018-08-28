1d71747d24cb1ee97a35b2208bc759386384d5c2
Hyepago <hyepago@naver.com>
2018-08-29 01:32:49 +0900

    viewpager _ page1

--- app/src/main/kotlin/com/emirim/hyejin/mokgongso/mandalart/CreateMandalart.kt
+++ app/src/main/kotlin/com/emirim/hyejin/mokgongso/mandalart/CreateMandalart.kt
@@ -1,33 +1,73 @@
 package com.emirim.hyejin.mokgongso.mandalart
 
+import android.content.Context
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.view.View
 
 import com.emirim.hyejin.mokgongso.R
 import com.emirim.hyejin.mokgongso.mandalart.page.Page1
 import com.emirim.hyejin.mokgongso.mandalart.page.Page2
 import com.emirim.hyejin.mokgongso.mandalart.page.Page3
 import kotlinx.android.synthetic.main.activity_mandalart_create.*
+import kotlinx.android.synthetic.main.activity_page_1.*
 
 class CreateMandalart : AppCompatActivity() {
     companion object {
         val MAX_PAGE = 3
         var curFragment = Fragment()
+        lateinit var mViewPager: ViewPager
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mandalart_create)
 
-        viewPager.adapter = adapter(supportFragmentManager)
+        mViewPager = viewPager
+        mViewPager.adapter = adapter(supportFragmentManager)
+
+        dot1.setImageResource(R.drawable.selected_dot)
+        dot2.setImageResource(R.drawable.default_dot)
+        dot3.setImageResource(R.drawable.default_dot)
+
+        mViewPager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener {
+            override fun onPageSelected(position: Int) {
+                when(position) {
+                    0 -> {
+                        dot1.setImageResource(R.drawable.selected_dot)
+                        dot2.setImageResource(R.drawable.default_dot)
+                        dot3.setImageResource(R.drawable.default_dot)
+                    }
+                    1 -> {
+                        dot1.setImageResource(R.drawable.default_dot)
+                        dot2.setImageResource(R.drawable.selected_dot)
+                        dot3.setImageResource(R.drawable.default_dot)
+                    }
+                    2 -> {
+                        dot1.setImageResource(R.drawable.default_dot)
+                        dot2.setImageResource(R.drawable.default_dot)
+                        dot3.setImageResource(R.drawable.selected_dot)
+                    }
+                }
+            }
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+            }
+        })
     }
 
     class adapter(fm: FragmentManager): FragmentPagerAdapter(fm) {
+
         override fun getItem(position: Int): Fragment {
             if(position < 0 || MAX_PAGE <= position)
                 return curFragment
--- app/src/main/kotlin/com/emirim/hyejin/mokgongso/mandalart/page/Page1.kt
+++ app/src/main/kotlin/com/emirim/hyejin/mokgongso/mandalart/page/Page1.kt
@@ -3,10 +3,16 @@ package com.emirim.hyejin.mokgongso.mandalart.page
 import android.os.Bundle
 import android.support.constraint.ConstraintLayout
 import android.support.v4.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.emirim.hyejin.mokgongso.R
+import com.emirim.hyejin.mokgongso.mandalart.CreateMandalart
+import kotlinx.android.synthetic.main.activity_mandalart_create.*
+import kotlinx.android.synthetic.main.activity_page_1.*
+import kotlinx.android.synthetic.main.activity_page_1.view.*
 
 class Page1 : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +23,25 @@ class Page1 : Fragment() {
         val constraintLayout = inflater.inflate(R.layout.activity_page_1, container, false) as ConstraintLayout
 
         constraintLayout.setBackgroundResource(R.color.backgroundColor)
+
+        constraintLayout.rightArrow.visibility = View.GONE
+
+        constraintLayout.mandalartTitle.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) { }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(s.toString().trim().isNotEmpty()) {
+                    rightArrow.visibility = View.VISIBLE
+                } else {
+                    rightArrow.visibility = View.GONE
+                }
+            }
+        })
+
+        constraintLayout.rightArrow.setOnClickListener {
+            CreateMandalart.mViewPager.currentItem = 1
+        }
+
 
         return constraintLayout
     }
