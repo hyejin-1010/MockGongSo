f1f35196b5ef3f68c7a3f1112142ccf0d7bb3d9d
Hyepago <hyepago@naver.com>
2018-08-31 00:31:35 +0900

    viewpager page 3 view

--- app/src/main/kotlin/com/emirim/hyejin/mokgongso/mandalart/page/Page3.kt
+++ app/src/main/kotlin/com/emirim/hyejin/mokgongso/mandalart/page/Page3.kt
@@ -1,14 +1,21 @@
 package com.emirim.hyejin.mokgongso.mandalart.page
 
 import android.os.Bundle
-import android.support.constraint.ConstraintLayout
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.emirim.hyejin.mokgongso.R
+import com.emirim.hyejin.mokgongso.mandalart.CreateMandalart
+import com.emirim.hyejin.mokgongso.mandalart.Mandalart
+import kotlinx.android.synthetic.main.activity_page_3.view.*
 
 public class Page3 : Fragment() {
+    companion object {
+        lateinit var mandalartSub: Array<TextView>
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -16,6 +23,21 @@ public class Page3 : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val constraintLayout = inflater.inflate(R.layout.activity_page_3, container, false)
 
+        mandalartSub = arrayOf(constraintLayout.secondTitle1, constraintLayout.secondTitle2, constraintLayout.secondTitle3, constraintLayout.secondTitle4, constraintLayout.secondTitle5, constraintLayout.secondTitle6, constraintLayout.secondTitle7,constraintLayout.secondTitle8)
+
+        constraintLayout.leftArrow.setOnClickListener {
+            CreateMandalart.mViewPager.currentItem = 1
+        }
+
         return constraintLayout
+    }
+
+    override fun setUserVisibleHint(isVisibleToUser: Boolean) {
+        super.setUserVisibleHint(isVisibleToUser)
+
+        for(i in 1..(Mandalart.count - 1)) {
+            mandalartSub[i - 1].setText("2")
+            mandalartSub[i - 1].setBackgroundResource(R.drawable.mandalart_box_1)
+        }
     }
 }
